3b7f3eb428d2a7de3dc0cf33993e15e28da80a6d
Timilehin Osiyoku <timmysolomon953@gmail.com>
2025-05-03 20:51:58 -0500

    Update: set NEXTAUTH_URL to production domain

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -9,6 +9,10 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  debug: true, // Optional: helps with debugging during development
+  pages: {
+    signIn: '/auth/signin', // Optional: Customize login page if you have one
+  },
 });
 
 export { handler as GET, handler as POST };
